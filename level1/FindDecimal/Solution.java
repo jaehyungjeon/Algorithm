f537159dc2a6d5468725e4e867f8df6c9855e84b
JAEHYUNG <jaehyung601@naver.com>
2022-07-26 09:10:08 +0900

    commit

--- level1/FindDecimal/Solution.java
+++ level1/FindDecimal/Solution.java
@@ -4,46 +4,33 @@ import java.util.*;
 
 /*
  * Question : 
-	1부터 입력받은 숫자 n 사이에 있는 소수의 개수를 반환하는 함수, solution을 만들어 보세요.
-	
-	소수는 1과 자기 자신으로만 나누어지는 수를 의미합니다.
-	(1은 소수가 아닙니다.)
+	길이가 n이고, "수박수박수박수...."와 같은 패턴을 유지하는 문자열을 리턴하는 함수, solution을 완성하세요. 
+	예를들어 n이 4이면 "수박수박"을 리턴하고 3이라면 "수박수"를 리턴하면 됩니다.
  * 
- * 	Solution : 1. 소수를 구할 때, 0, 1은 소수가 아니므로 0으로 셋팅
- * 			   2. 제곱근 만큼 for문을 돌리고 안쪽에서 다시금 i의 배수만큼 돌려 존재하면 0으로 배열 셋팅
- * 			   3. 최종 결괏값 중 배열에 0이 들어가지 않은 것의 갯수 만큼 ++
+ * 	Solution : 1. 2로 나눠서 나머지 0 : 수 / 1 : 박
  *  
  * */
 
 public class Solution {
 	
-    public int solution(int n) {
-	   int answer = 0;
-       
-	   int[] arr = new int[n+1];
-	   arr[0] = arr[1] = 0;
-	   
-	   for(int i=2; i<=n; i++) {
-		   arr[i] = i;
-	   }
-	   
-	   for(int i=2; i<=(int)Math.sqrt(n); i++) {
-		   if(arr[i] == 0) continue;
-		   for(int j=2*i; j<=n; j+=i) {
-			   arr[j] = 0;
-		   }
-	   }
-	   
-	   for(int i=0; i<arr.length; i++) {
-		   if(arr[i] != 0) answer++;
-	   }
-       return answer;
+    public String solution(int n) {
+        String answer = "";
+        
+        for(int i=0; i<n; i++) {
+        	if(i%2 == 0) {
+        		answer += "수";
+        	} else {
+        		answer += "박";
+        	}
+        }
+        
+        return answer;
     }
     
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Solution sol = new Solution();
-    	int n = 10;
+    	int n = 3;
 		System.out.println(sol.solution(n));
 	}
 }
