e8dab366583f3c6ecba5be55ac906c38aefc8b4a
JAEHYUNG <jaehyung601@naver.com>
2022-07-26 09:17:17 +0900

    commit

---
+++ level1/StringToInteger/Solution.java
@@ -0,0 +1,25 @@
+package StringToInteger;
+
+import java.util.*;
+
+/*
+ * Question : 
+	문자열 s를 숫자로 변환한 결과를 반환하는 함수, solution을 완성하세요.
+ * 
+ * 	Solution : 1. parsing
+ *  
+ * */
+
+public class Solution {
+	
+    public int solution(String s) {
+        return Integer.parseInt(s);
+    }
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Solution sol = new Solution();
+    	String s = "1234";
+		System.out.println(sol.solution(s));
+	}
+}
