5ffc2a0b76e85823f98a92132fd773ba458c8017
JAEHYUNG <jaehyung601@naver.com>
2022-08-18 09:58:34 +0900

    commit

--- src/Level2/Hopscotch/Solution.java
+++ src/Level2/Hopscotch/Solution.java
@@ -21,22 +21,32 @@ import java.util.*;
 	위 예의 경우, 1행의 네번째 칸 (5), 2행의 세번째 칸 (7), 3행의 첫번째 칸 (4) 땅을 밟아 16점이 최고점이 되므로 16을 return 하면 됩니다.
  * 
  * 
- * 	Solution : 1. 
+ * 	Solution : 1. dfs로 접근해봤으나, 랜딩의 고정값에 이슈
+ * 			   2. 수동으로 배열마다 접점이 되지 않는 즉, 0번째 배열 -> 1, 2, 3 값들의 최댓값을 뽑아 데이터를 넣는 방식으로 배열 구성
+ * 			   3. 오름차순으로 정렬 후, 제일 마지막 값 -> max값이므로 return
  * 
  * */
 
 public class Solution {
 
     int solution(int[][] land) {
-        int answer = 0;
-
-        return answer;
+		for(int i=1; i<land.length; i++) {
+			land[i][0] += Math.max(Math.max(land[i-1][1], land[i-1][2]), land[i-1][3]);
+			land[i][1] += Math.max(Math.max(land[i-1][0], land[i-1][2]), land[i-1][3]);
+			land[i][2] += Math.max(Math.max(land[i-1][1], land[i-1][0]), land[i-1][3]);
+			land[i][3] += Math.max(Math.max(land[i-1][1], land[i-1][2]), land[i-1][0]);
+		}
+        
+		int[] answer = land[land.length-1];
+		Arrays.sort(answer);
+		
+        return answer[answer.length-1];
     }
     
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Solution sol = new Solution();
-		int[] arr = {1,2,3};
-		System.out.println(sol.solution(arr));
+		int[][] land = {{1,2,3,5},{5,6,7,8},{4,3,2,1}};
+		System.out.println(sol.solution(land));
 	}
 }
