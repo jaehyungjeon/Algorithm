67f2081e9d2e36fdedef3203550a3039a68f2532
JAEHYUNG <jaehyung601@naver.com>
2023-09-04 17:18:42 +0900

    commit

---
+++ src/BaekJoon/BaekJoon_1809.java
@@ -0,0 +1,34 @@
+package BaekJoon;
+
+/*
+	Question : You’ve decided to buy a farm and start a new life. 
+	To pass some time while you wait for the title of the land to go through, produce the following ascii cow.
+	 	
+	input			 
+	
+	output
+	(___)
+	(o o)____/
+	 @@      \
+	  \ ____,/
+	  //   //
+	 ^^   ^^
+	 
+	Solution : 1. 단순 프린트 함수... 대신 문제는 golfScript라 "" 안에 넣어주면 끝.
+	
+*/
+
+public class BaekJoon_1809 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(
+				"(___)\r\n"
+				+ "(o o)____/\r\n"
+				+ " @@      \\\r\n"
+				+ "  \\ ____,/\r\n"
+				+ "  //   //\r\n"
+				+ " ^^   ^^");
+	}
+
+}
