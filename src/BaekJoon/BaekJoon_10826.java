c91385cd01bca0673ca77dd97f39203ce05a33a0
JAEHYUNG <jaehyung601@naver.com>
2023-09-06 10:12:39 +0900

    commit

---
+++ src/BaekJoon/BaekJoon_10826.java
@@ -0,0 +1,45 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+/*
+	Question : 피보나치 수는 0과 1로 시작한다. 0번째 피보나치 수는 0이고, 1번째 피보나치 수는 1이다. 그 다음 2번째 부터는 바로 앞 두 피보나치 수의 합이 된다.
+
+	이를 식으로 써보면 Fn = Fn-1 + Fn-2 (n ≥ 2)가 된다.
+	
+	n=17일때 까지 피보나치 수를 써보면 다음과 같다.
+	
+	0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597
+	
+	n이 주어졌을 때, n번째 피보나치 수를 구하는 프로그램을 작성하시오.
+
+	input			 
+	10
+	
+	output
+	55
+	
+	Solution : 1. BigInteger를 활용해야 시간초과 안나옴.
+			   2. 점화식 : dp[M] = dp[M-1] + dp[M-2];
+
+*/
+
+public class BaekJoon_10826 {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		BigInteger[] dp = new BigInteger[N+2];
+		dp[0] = new BigInteger("0");
+		dp[1] = new BigInteger("1");
+		
+		for(int i=2; i<dp.length; i++) {
+			dp[i] = dp[i-1].add(dp[i-2]);
+		}
+		
+		System.out.println(dp[N]);
+	}
+}
