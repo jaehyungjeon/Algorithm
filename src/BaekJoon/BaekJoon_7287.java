2d4810808f190404e6b9cbb07df924d535bfdc8d
JAEHYUNG <jaehyung601@naver.com>
2023-09-12 17:28:10 +0900

    commit

---
+++ src/BaekJoon/BaekJoon_7287.java
@@ -0,0 +1,24 @@
+package BaekJoon;
+
+/*
+	Question : 자신이 백준 온라인 저지(BOJ)에서 맞은 문제의 수와 아이디를 그대로 출력하는 프로그램을 작성하시오.
+	
+	input			 
+	
+	output
+	123
+	Your_ICPC_Team_Name
+	
+	Solution : 1. 그냥 단순 출력
+	
+*/
+
+public class BaekJoon_7287 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(306);
+		System.out.println("jaehyung601");
+	}
+
+}
