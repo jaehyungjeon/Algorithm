e81ad64ed0e082e9512e721c8d1b6b5f1b0f8160
JAEHYUNG <jaehyung601@naver.com>
2023-09-15 08:54:37 +0900

    commit

---
+++ src/BaekJoon/BaekJoon_5339.java
@@ -0,0 +1,40 @@
+package BaekJoon;
+
+/*
+	Question : 스타워즈에 등장하는 로봇인 C3PO는 요즘 콜센터에 근무하고 있다. 콜센터에 앉아있는 C3PO를 그리는 프로그램을 작성하시오.
+
+	input
+	
+	output
+     	/~\
+	    ( oo|
+	    _\=/_
+	   /  _  \
+	  //|/.\|\\
+	 ||  \ /  ||
+	============
+	|          |
+	|          |
+	|          |
+	
+	Solution : 1. 단순출력
+
+*/
+
+public class BaekJoon_5339 {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("     /~\\");
+		System.out.println("    ( oo|");
+		System.out.println("    _\\=/_");
+		System.out.println("   /  _  \\");
+		System.out.println("  //|/.\\|\\\\"); 
+		System.out.println(" ||  \\ /  ||");
+		System.out.println("============");
+		System.out.println("|          |");
+		System.out.println("|          |");
+		System.out.print("|          |");
+	}
+
+}
