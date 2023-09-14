03509772fbb94a134e2a8595d25a6bbbe70f5606
JAEHYUNG <jaehyung601@naver.com>
2023-09-14 15:20:32 +0900

    commit

---
+++ src/BaekJoon/BaekJoon_5337.java
@@ -0,0 +1,26 @@
+package BaekJoon;
+
+/*
+	Question : Welcome을 예제 출력처럼 출력하는 프로그램을 작성하시오.
+
+	input			 
+	
+	output
+	.  .   .
+	|  | _ | _. _ ._ _  _
+	|/\|(/.|(_.(_)[ | )(/.
+	
+	Solution : 1. 그대로 출력
+
+*/
+
+public class BaekJoon_5337 {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println(".  .   .");
+		System.out.println("|  | _ | _. _ ._ _  _");
+		System.out.print("|/\\|(/.|(_.(_)[ | )(/.");
+	}
+
+}
