50ae8cb9a784cac8805b7cfe964c34840155d01d
JAEHYUNG <jaehyung601@naver.com>
2023-09-14 14:58:40 +0900

    commit

---
+++ src/BaekJoon/BaekJoon_11942.java
@@ -0,0 +1,22 @@
+package BaekJoon;
+
+/*
+	Question : 고려대학교는 사랑입니다.
+
+	input			 
+	
+	output
+	고려대학교
+	
+	Solution : 1. 그대로 출력
+
+*/
+
+public class BaekJoon_11942 {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.print("고려대학교");
+	}
+
+}
