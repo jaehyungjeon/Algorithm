1663aa0532294dae12ccd583b48007ffa78ab9f7
JAEHYUNG <jaehyung601@naver.com>
2023-09-14 14:39:58 +0900

    commit

---
+++ src/BaekJoon/BaekJoon_10797.java
@@ -0,0 +1,45 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+	Question : 서울시는 6월 1일부터 교통 혼잡을 막기 위해서 자동차 10부제를 시행한다. 
+	자동차 10부제는 자동차 번호의 일의 자리 숫자와 날짜의 일의 자리 숫자가 일치하면 해당 자동차의 운행을 금지하는 것이다. 
+	예를 들어, 자동차 번호의 일의 자리 숫자가 7이면 7일, 17일, 27일에 운행하지 못한다. 
+	또한, 자동차 번호의 일의 자리 숫자가 0이면 10일, 20일, 30일에 운행하지 못한다.
+
+	여러분들은 일일 경찰관이 되어 10부제를 위반하는 자동차의 대수를 세는 봉사활동을 하려고 한다. 
+	날짜의 일의 자리 숫자가 주어지고 5대의 자동차 번호의 일의 자리 숫자가 주어졌을 때 위반하는 자동차의 대수를 출력하면 된다. 
+
+	input			 
+	3
+	1 2 3 5 3
+	
+	output
+	2
+	
+	Solution : 1. for문 + count  
+
+*/
+
+public class BaekJoon_10797 {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int count = 0;
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0; i<5; i++) {
+			int value = Integer.parseInt(st.nextToken());
+			
+			if(value == N) count++;
+		}
+		
+		System.out.println(count);
+	}
+
+}
