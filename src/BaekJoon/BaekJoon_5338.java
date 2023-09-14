aa8fbb50e682b28e45dd826a665a444966710354
JAEHYUNG <jaehyung601@naver.com>
2023-09-14 13:41:34 +0900

    commit

---
+++ src/BaekJoon/BaekJoon_5338.java
@@ -0,0 +1,29 @@
+package BaekJoon;
+
+/*
+	Question : 마이크로소프트 로고를 예제 출력처럼 출력하는 프로그램을 작성하시오.
+
+	input			 
+	
+	output
+	       _.-;;-._
+	'-..-'|   ||   |
+	'-..-'|_.-;;-._|
+	'-..-'|   ||   |
+	'-..-'|_.-''-._|
+	
+	Solution : 1. 라인별로 끊지 않으면 출력형식 에러 
+
+*/
+
+public class BaekJoon_5338 {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("       _.-;;-._");
+        System.out.println("'-..-'|   ||   |");
+        System.out.println("'-..-'|_.-;;-._|");
+        System.out.println("'-..-'|   ||   |");
+        System.out.print("'-..-'|_.-''-._|");
+	}
+}
