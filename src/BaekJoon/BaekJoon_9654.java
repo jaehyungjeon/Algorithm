faadfa895d740ecfc5a3aed073880c977ab4052d
JAEHYUNG <jaehyung601@naver.com>
2023-09-15 09:05:38 +0900

    commit

---
+++ src/BaekJoon/BaekJoon_9654.java
@@ -0,0 +1,32 @@
+package BaekJoon;
+
+/*
+	Question : 나부 행성의 함대 정보를 출력하는 프로그램을 작성하시오.
+
+	input
+	
+	output
+	SHIP NAME      CLASS          DEPLOYMENT IN SERVICE
+	N2 Bomber      Heavy Fighter  Limited    21        
+	J-Type 327     Light Combat   Unlimited  1         
+	NX Cruiser     Medium Fighter Limited    18        
+	N1 Starfighter Medium Fighter Unlimited  25        
+	Royal Cruiser  Light Combat   Limited    4         
+	
+	Solution : 1. 단순 출력
+
+*/
+
+public class BaekJoon_9654 {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("SHIP NAME      CLASS          DEPLOYMENT IN SERVICE");
+		System.out.println("N2 Bomber      Heavy Fighter  Limited    21        ");
+		System.out.println("J-Type 327     Light Combat   Unlimited  1         ");
+		System.out.println("NX Cruiser     Medium Fighter Limited    18        ");
+		System.out.println("N1 Starfighter Medium Fighter Unlimited  25        ");
+		System.out.print("Royal Cruiser  Light Combat   Limited    4         ");
+	}
+
+}
