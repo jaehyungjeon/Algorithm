d4264dd2c41a8367ef45f88ce2ca5da426b4efce
JAEHYUNG <jaehyung601@naver.com>
2023-09-15 13:45:50 +0900

    commit

---
+++ src/BaekJoon/BaekJoon_2372.java
@@ -0,0 +1,45 @@
+package BaekJoon;
+
+/*
+	Question : Print a table that describes the current count of all your livestock.
+
+	input			 
+	
+	output
+	Animal      Count
+	-----------------
+	Chickens      100
+	Clydesdales     5
+	Cows           40
+	Goats          22
+	Steers          2
+	
+	Solution : 1. 단순 출력
+			   2. ada를 사용해야 함. 주석처리 해놨음.
+	
+*/
+
+public class BaekJoon_2372 {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Animal      Count");
+		System.out.println("-----------------");
+		System.out.println("Chickens      100");
+		System.out.println("Clydesdales     5");
+		System.out.println("Cows           40");
+		System.out.println("Goats          22");
+		System.out.print("Steers          2");
+		
+		/*
+		 * with Ada.Text_IO; use Ada.Text_IO;
+		 * 
+		 * procedure program_alioolio is begin Put_Line("Animal      Count");
+		 * Put_Line("-----------------"); Put_Line("Chickens      100");
+		 * Put_Line("Clydesdales     5"); Put_Line("Cows           40");
+		 * Put_Line("Goats          22"); Put_Line("Steers          2"); end
+		 * program_alioolio;
+		 */
+	}
+
+}
