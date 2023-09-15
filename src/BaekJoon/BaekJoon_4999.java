803075203447ac27801122abf67ddcc28e6550e6
JAEHYUNG <jaehyung601@naver.com>
2023-09-15 09:15:20 +0900

    commit

---
+++ src/BaekJoon/BaekJoon_4999.java
@@ -0,0 +1,50 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/*
+	Question : 재환이는 저스틴 비버 콘서트에서 소리를 너무 많이 질러서 인후염에 걸렸다.
+
+	의사는 재환이에게 "aaah"를 말해보라고 시켰다. 안타깝게도 재환이는 의사가 원하는만큼 소리를 길게 낼 수 없는 경우가 있었다.
+	
+	각각의 의사는 재환이에게 특정한 길이의 "aah"를 말해보라고 요청한다. 어떤 의사는 "aaaaaah"를 요구하기도 하고, "h"만 요구하는 의사도 있다.
+	
+	모든 의사는 자신이 원하는 길이의 "aah"를 듣지 못하면 진단을 내릴 수 없다.
+	
+	따라서, 재환이는 집에서 자신이 얼마나 길게 "aah"를 낼 수 있는지 알아냈고, 자기가 소리낼 수 있는 길이의 "aah"를 요구하는 의사를 방문하려고 한다.
+	
+	재환이가 낼 수 있는 "aah"의 길이와 의사가 요구하는 길이가 주어진다. 이때, 그 병원에 가야하는지 말아야하는지를 알아내는 프로그램을 작성하시오.
+
+	input
+	aaah
+	aaaaah
+	
+	output
+	no 
+	
+	Solution : 1. 단순하게 길이만 비교해주면 된다.
+
+*/
+
+public class BaekJoon_4999 {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		String str = br.readLine();
+		
+		int sLen = s.length();
+		int strLen = str.length();
+		
+		if(sLen >= strLen) {
+			System.out.println("go");
+		} else {
+			System.out.println("no");
+		}
+		
+		
+	}
+
+}
