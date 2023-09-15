ab3c78b8a51102ddac244d7a126e402f9b9f7313
JAEHYUNG <jaehyung601@naver.com>
2023-09-15 09:08:28 +0900

    commit

---
+++ src/BaekJoon/BaekJoon_10170.java
@@ -0,0 +1,47 @@
+package BaekJoon;
+
+/*
+	Question : 동혁이를 위해 NFC 서부와 북부 디비전 순위를 출력하는 프로그램을 작성하시오.
+
+	input
+	
+	output
+	NFC West       W   L  T
+	-----------------------
+	Seattle        13  3  0
+	San Francisco  12  4  0
+	Arizona        10  6  0
+	St. Louis      7   9  0
+	
+	NFC North      W   L  T
+	-----------------------
+	Green Bay      8   7  1
+	Chicago        8   8  0
+	Detroit        7   9  0
+	Minnesota      5  10  1   
+	
+	Solution : 1. 단순 출력
+
+*/
+
+public class BaekJoon_10170 {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("NFC West       W   L  T");
+		System.out.println("-----------------------");
+		System.out.println("Seattle        13  3  0");
+		System.out.println("San Francisco  12  4  0");
+		System.out.println("Arizona        10  6  0");
+		System.out.println("St. Louis      7   9  0");
+		System.out.println();
+		System.out.println("NFC North      W   L  T");
+		System.out.println("-----------------------");
+		System.out.println("Green Bay      8   7  1");
+		System.out.println("Chicago        8   8  0");
+		System.out.println("Detroit        7   9  0");
+		System.out.print("Minnesota      5  10  1");
+		
+	}
+
+}
