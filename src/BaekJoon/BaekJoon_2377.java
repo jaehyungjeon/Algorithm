8ff776e74423ec87bdce54b7dddf439b792d40d1
JAEHYUNG <jaehyung601@naver.com>
2023-09-15 14:10:15 +0900

    commit

---
+++ src/BaekJoon/BaekJoon_2377.java
@@ -0,0 +1,50 @@
+package BaekJoon;
+
+/*
+	Question : 3D printing is becoming more and more prominent in today’s society. Unfortunately, you don’t currently have access to a 3D printer. 
+	Instead, you can just print an item with your knowledge of coding! Print a clay pot.
+
+	input			 
+
+	output
+	 _________
+	 \_     _/
+	   \   /
+	    | |
+	   /   \
+	  /     \
+	 |       |
+	/---------\
+	| \-/ \-/ |
+	\---------/
+	 \_______/
+	
+	Solution : 1. 단순 출력
+			   2. freeBasic 사용 -> 주석처리
+	
+*/
+
+public class BaekJoon_2377 {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println(" _________");
+		System.out.println(" \\_     _/");
+		System.out.println("   \\   /");
+		System.out.println("    | |");
+		System.out.println("   /   \\");
+		System.out.println("  /     \\");
+		System.out.println(" |       |");
+		System.out.println("/---------\\");
+		System.out.println("| \\-/ \\-/ |");
+		System.out.println("\\---------/");
+		System.out.print(" \\_______/");
+		
+		/*
+		 * PRINT " _________" PRINT " \_     _/" PRINT "   \   /" PRINT "    | |" PRINT
+		 * "   /   \" PRINT "  /     \" PRINT " |       |" PRINT "/---------\" PRINT
+		 * "| \-/ \-/ |" PRINT "\---------/" PRINT " \_______/"
+		 */
+	}
+
+}
