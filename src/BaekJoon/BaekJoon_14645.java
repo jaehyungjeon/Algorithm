08db43319ef6de2ce14fd9d60e9e9e47a59f204d
JAEHYUNG <jaehyung601@naver.com>
2023-09-15 09:20:55 +0900

    commit

---
+++ src/BaekJoon/BaekJoon_14645.java
@@ -0,0 +1,27 @@
+package BaekJoon;
+
+/*
+	Question : 버스 운전수 비와이 씨가 운전하는 버스(verse아님 ㅎ)는 N개의 정거장을 거친 후 종착역에 도착한다. 
+	각 정거장은 내릴 인원수와 올라탈 인원수가 정해져 있다. 종착역에 도착하면 버스에 타고 있던 모든 사람이 내린다.
+
+	input
+	3 2
+	10 3
+	21 8
+	0 15
+	
+	output
+	비와이
+	
+	Solution : 1. 입력이 있어도 운전수만 출력하면 됨.
+
+*/
+
+public class BaekJoon_14645 {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("비와이");
+	}
+
+}
