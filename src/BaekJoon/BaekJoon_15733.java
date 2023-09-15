241136c7afcfe55ac0fb48b23d0d9ed3490990a7
JAEHYUNG <jaehyung601@naver.com>
2023-09-15 09:22:33 +0900

    commit

---
+++ src/BaekJoon/BaekJoon_15733.java
@@ -0,0 +1,22 @@
+package BaekJoon;
+
+/*
+	Question : 당신은 지금 코딩을 하고있다. 그럼 당신은 어떤 사람인가.
+
+	input
+	
+	output
+	I'm Sexy
+	
+	Solution : 1. 단순 출력
+
+*/
+
+public class BaekJoon_15733 {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.print("I'm Sexy");
+	}
+
+}
