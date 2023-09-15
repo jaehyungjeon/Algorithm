9c4d3e30768087c32911cf5e61fa310c5156c206
JAEHYUNG <jaehyung601@naver.com>
2023-09-15 12:41:15 +0900

    commit

---
+++ src/BaekJoon/BaekJoon_10189.java
@@ -0,0 +1,28 @@
+package BaekJoon;
+
+/*
+	Question : Print out the word Hook as shown below.
+
+	input			 
+	
+	output
+	#  # #### #### #  #
+	#### #  # #  # # #
+	#### #  # #  # # #
+	#  # #### #### #  #
+	
+	Solution : 1. 단순 출력
+	
+*/
+
+public class BaekJoon_10189 {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("#  # #### #### #  #");
+		System.out.println("#### #  # #  # # #");
+		System.out.println("#### #  # #  # # #");
+		System.out.print("#  # #### #### #  #");
+	}
+
+}
